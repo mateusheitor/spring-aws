e81fdfef2d6de00eea629ecc7ff6e61df05a6f56
mateu <mateu@192.168.1.2>
2023-07-21 17:40:08 -0300

    Task ID #v0

--- src/main/java/com/spring/codeblog/CodeblogApplication.java
+++ src/main/java/com/spring/codeblog/CodeblogApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class CodeblogApplication {
 
 	public static void main(String[] args) {
+		//v0
 		SpringApplication.run(CodeblogApplication.class, args);
 	}
 
