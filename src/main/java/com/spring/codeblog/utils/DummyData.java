a8316fa8c0431b6e99dc28306435bb73761ab52d
mateu <mateu@192.168.1.2>
2023-07-22 12:55:57 -0300

    Ajustes

--- src/main/java/com/spring/codeblog/utils/DummyData.java
+++ src/main/java/com/spring/codeblog/utils/DummyData.java
@@ -18,20 +18,20 @@ public class DummyData {
 	@Autowired
 	CodeblogRepository codeblogRepository;
 	
-	//@PostConstruct
+	@PostConstruct
 	public void savePosts() {
 		List<Post> postList = new ArrayList<>();
 		Post post1 = new Post();
 		post1.setAutor("Carvalho");
 		post1.setData(LocalDate.now());
 		post1.setTitulo("Bastardos");
-		post1.setTexto("klasjdklsa jksdlasjdla");
+		post1.setTexto("\"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.\"");
 		
 		Post post2 = new Post();
 		post2.setAutor("Armando");
 		post2.setData(LocalDate.now());
 		post2.setTitulo("Livres");
-		post2.setTexto("klasjdklsa jksdlasjdla");
+		post2.setTexto("\"Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?\"");
 		
 		postList.add(post1);
 		postList.add(post2);
